13ae5e3d5634b1a7f1c095c6aab2a56e772e00d2
Aliaksandr Pasynkau <alxe.master@gmail.com>
2014-06-23 02:21:50 +0300

    add deploy task

---
+++ grunt/website/deploy.js
@@ -0,0 +1,89 @@
+"use strict";
+
+module.exports = function (grunt) {
+	var opt = this,
+		NAME = this.lnk(),
+		SRC = this.lnk(opt.SRC),
+		DEPLOY = this.lnk(opt.DEPLOY),
+		BUILD = this.lnk(opt.BUILD);
+
+	this
+		.less({
+			options: {
+				compress: true,
+				ieCompat: true
+			},
+			files: [
+				{
+					expand: true,
+					overwrite: true,
+					src: [
+						BUILD + '/**/*.css'
+					]
+				}
+			]
+		})
+
+		.clean('deploy', [
+			DEPLOY
+		])
+
+		.copy('2deploy', {
+			files: [{
+				expand: true,
+				cwd: BUILD,
+				src: [ '**/*' ],
+				dest: opt.DEPLOY + '/' + NAME
+			}]
+		})
+	;
+
+
+	var _ = require('lodash');
+
+	var incOptions = {
+		cwd: BUILD + '/static/js',
+		rename: function(base, opt) {
+			return opt.replace(/\.js$/, '');
+		}
+	};
+
+	var include = _(grunt.file.expandMapping(['controllers/**/*.js'], '', incOptions)).pluck('dest');
+
+	this
+		.requirejs({
+			options: {
+				appDir: BUILD + '/static',
+				baseUrl: 'js',
+				dir: opt.DEPLOY + '/' + NAME + '/static',
+				modules: [{
+					name: 'main',
+					include: include,
+					insertRequire: ['main']
+				}],
+				mainConfigFile: opt.DEPLOY + '/' + NAME + '/static/js/config.js',
+				almond: true,
+				optimize: 'uglify2',
+				useStrict: true,
+				normalizeDirDefines: 'skip',
+				keepBuildDir: false,
+				preserveLicenseComments: false,
+				inlineText: false
+			}
+		})
+
+		.clean('build', [
+			BUILD
+		])
+
+		.copy('2build', {
+			files: [{
+				expand: true,
+				cwd: opt.DEPLOY + '/' + NAME,
+				src: [ '**/*' ],
+				dest: BUILD
+			}]
+		})
+	;
+
+};
